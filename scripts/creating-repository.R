3a3603119d2a21a021848eb52a5a57dae8d4bea1
m-pereira <mateusmachadodepereira@gmail.com>
2022-11-11 23:49:34 -0300

    first commit

---
+++ scripts/creating-repository.R
@@ -0,0 +1,11 @@
+usethis::create_project("Github/my-blog")
+usethis::use_git()
+usethis::use_git_config(user.name = "m-pereira")
+gitcreds::gitcreds_set()
+usethis::edit_r_profile()
+usethis::use_github("m-pereira")
+install.packages("blogdown")
+blogdown::new_site(".",
+                   theme = "lxndrblz/anatole",
+                   netlify = F, format = "toml")
+
